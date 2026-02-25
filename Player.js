32bb53b1ca87ecb508a4d7d0579e6239e5480644
Jangho Son <json66@rrc.ca>
2026-02-25 17:42:39 -0600

    Update Player class: player slowly stops when no key pressed, add hyperjump function

--- Player.js
+++ Player.js
@@ -3,24 +3,43 @@ class Player extends BaseActor {
     super(30, position, createVector(0, 0));
     this.rotateSpeed = 0.1;
     this.angle = 0;
+    this.isMoving = false;
+    this.friction = 0.95;
+    this.spaceHeld = false;
   }
 
   update() {
     this.handleInput();
+
+    this.velocity.mult(this.friction);
+
     super.update();
   }
 
   handleInput() {
+    // Rotation
     if (keyIsDown(LEFT_ARROW)) {
       this.angle -= this.rotateSpeed;
     }
     if (keyIsDown(RIGHT_ARROW)) {
       this.angle += this.rotateSpeed;
     }
+
+    // Acceleration
     if (keyIsDown(UP_ARROW)) {
       let force = p5.Vector.fromAngle(this.angle);
       force.mult(0.2);
       this.velocity.add(force);
+    }
+
+    // HyperJump
+    if (keyIsDown(32) && !this.spaceHeld) {
+      this.position.x = random(width);
+      this.position.y = random(height);
+      this.spaceHeld = true;
+    }
+    if (!keyIsDown(32)) {
+      this.spaceHeld = false;
     }
   }
 
